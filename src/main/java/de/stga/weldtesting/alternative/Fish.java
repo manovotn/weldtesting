0eeb55aaea2322cfa2c86909df4bcf49ab1461d4
JavaTesting1 <40684593+JavaTesting1@users.noreply.github.com>
2018-12-18 10:56:50 +0100

    Added testcase. Altered default value from Fish.numberOfLegs 0 -> 1000

--- src/main/java/de/stga/weldtesting/alternative/Fish.java
+++ src/main/java/de/stga/weldtesting/alternative/Fish.java
@@ -10,7 +10,7 @@ public class Fish implements Serializable {
     private static final long serialVersionUID = 1L;
 
     public int numberOfLegs() {
-        return 0;
+        return 1000;
     }
 
 }
---
+++ src/test/java/de/stga/weldtesting/alternative/ManualWeldcontainer2.java
@@ -0,0 +1,40 @@
+package de.stga.weldtesting.alternative;
+
+import javax.enterprise.context.SessionScoped;
+import javax.enterprise.inject.spi.Bean;
+import javax.inject.Inject;
+
+import org.hamcrest.CoreMatchers;
+import org.jboss.weld.junit.MockBean;
+import org.jboss.weld.junit5.WeldInitiator;
+import org.jboss.weld.junit5.WeldJunit5Extension;
+import org.jboss.weld.junit5.WeldSetup;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@ExtendWith(WeldJunit5Extension.class)
+public class ManualWeldcontainer2 {
+
+    // Here the fish mock won`t overwrite the Fish class.
+
+    @WeldSetup
+    public WeldInitiator weld = WeldInitiator.from(WeldInitiator.createWeld().enableDiscovery()).addBeans(createFishBean())
+            .activate(SessionScoped.class).build();
+
+    @Inject
+    private FishTank classUnderTest;
+
+    static Bean<?> createFishBean() {
+        return MockBean.builder().types(Fish.class).scope(SessionScoped.class).selectedAlternative(Fish.class)
+                .creating(Mockito.when(Mockito.mock(Fish.class).numberOfLegs()).thenReturn(400).getMock()).build();
+    }
+
+    @Test
+    public void testNumberOfLegs1() {
+        assertThat(this.classUnderTest.numberOfFishLegs(), CoreMatchers.is(400));
+    }
+
+}
